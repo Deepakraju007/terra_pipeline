62dbf66a4aaeaf994f5f8f0bf4dc690e51c1b024
Raju Deepak <rajudeepak123@gmail.com>
2026-08-01 16:03:30 +0530

    modify commit

--- Env/dev/main.tf
+++ Env/dev/main.tf
@@ -1,15 +1,15 @@
 module "ec2_instance" {
   source = "../../modules/ec2_instance"
 
-  instance_name = "my-ec2-instance"
+  instance_name = "dev"
   ami_id        = "ami-02b64aa047cb5edf5" # Replace with your desired AMI ID
   instance_type = "t2.micro"
   
 }
 
 
-module "vpc" {
-  source      = "../../modules/vpc"
+module "aws_vpc" {
+  source      = "../../modules/aws_vpc"
   env         = "dev"
 
   cidr_block  = "10.0.0.0/16"   
