598c45bafb4a083dcba7117648f568f6f20f3c5e
Raju Deepak <rajudeepak123@gmail.com>
2026-08-03 12:46:35 +0530

    resolved merge 2

--- Env/dev/backend.tf
+++ Env/dev/backend.tf
@@ -6,20 +6,14 @@ terraform {
     }
   }
   backend "s3" {
-<<<<<<< HEAD
-    bucket = "terrabucket0801"
-    key    = "env/dev/terraform.tfstate"
+    bucket = "terrabucket0801 "
+    key    = "Env/dev/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform_infra"
-=======
-    bucket = "siva-kops-state-07-07-2026"
-    key    = "env/dev/terraform.tfstate"
-    region = "us-east-1"
->>>>>>> ce0a3df57fcf48919785e540eadf60676f66fe46
   }
 }
 
-# Configure the AWS Provider
+# Configure the AWS Provider  
 provider "aws" {
   region = var.aws_region
 }
--- Env/dev/main.tf
+++ Env/dev/main.tf
@@ -5,11 +5,7 @@ module "ec2" {
   ami            = "ami-02b64aa047cb5edf5" 
   env            = "dev"
   instance_type  = "t3.micro"
-<<<<<<< HEAD
   instance_count = 3
-=======
-  instance_count = 2
->>>>>>> ce0a3df57fcf48919785e540eadf60676f66fe46
   subnet_id      = module.vpc.subnet_id
 }
 
