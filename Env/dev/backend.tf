70832fdb2d70d4cddb31ac5e1b0cd5a74ac8b020
Deepakraju007 <37394734+Deepakraju007@users.noreply.github.com>
2026-08-03 11:25:50 +0530

    Configure S3 backend for Terraform state

--- Env/dev/backend.tf
+++ Env/dev/backend.tf
@@ -5,9 +5,15 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "s3" {
+    bucket = "siva-kops-state-07-07-2026"
+    key    = "env/dev/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform_infra"
+  }
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = var.aws_region
 }
