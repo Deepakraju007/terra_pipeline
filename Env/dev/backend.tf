80a86c70d81b061dd6c6e576f65bf806d11d3ac7
Raju Deepak <rajudeepak123@gmail.com>
2026-08-03 12:47:00 +0530

    resolved merge 3

--- Env/dev/backend.tf
+++ Env/dev/backend.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "terrabucket0801 "
+    bucket = "terrabucket0801"
     key    = "Env/dev/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform_infra"
